90116c288dd3d2ce9253287e64e62ae580904671
mtalalned <muhammadtalalned@gmail.com>
2024-10-05 22:59:21 +0500

    card assignment completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import CARDBODY from "./components/Card";
 function App (){
   return (
     <>
-    <CARDBODY imgsrc="" price="" description="" totalRunning="" model="" location="" adPosted=""/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/490095978-800x600.webp" price="1,880,000" description="Suzuki Every Wagon 2011" totalRunning="137,000 km" model="2011" location="Cantt, Karachi" adPosted="5 days ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/486403677-800x600.webp" price="2,100,000" description="Toyota Vitz 2013" totalRunning="170,000" model="2013" location="Shah Rukn-e-Alam Colony, Multan" adPosted="2 days ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/491109304-800x600.webp" price="820,000" description="Suzuki Cultus VXL 2005" totalRunning="80,000" model="2005" location="Gulistan-e-Jauhar Block 12, Karachi" adPosted="1 day ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/490095978-800x600.webp" price="1,880,000" description="Suzuki Every Wagon 2011" totalRunning="137,000 km" model="2011" location="Cantt, Karachi" adPosted="5 days ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/486403677-800x600.webp" price="2,100,000" description="Toyota Vitz 2013" totalRunning="170,000" model="2013" location="Shah Rukn-e-Alam Colony, Multan" adPosted="2 days ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/491109304-800x600.webp" price="820,000" description="Suzuki Cultus VXL 2005" totalRunning="80,000" model="2005" location="Gulistan-e-Jauhar Block 12, Karachi" adPosted="1 day ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/490095978-800x600.webp" price="1,880,000" description="Suzuki Every Wagon 2011" totalRunning="137,000 km" model="2011" location="Cantt, Karachi" adPosted="5 days ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/486403677-800x600.webp" price="2,100,000" description="Toyota Vitz 2013" totalRunning="170,000" model="2013" location="Shah Rukn-e-Alam Colony, Multan" adPosted="2 days ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/491109304-800x600.webp" price="820,000" description="Suzuki Cultus VXL 2005" totalRunning="80,000" model="2005" location="Gulistan-e-Jauhar Block 12, Karachi" adPosted="1 day ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/490095978-800x600.webp" price="1,880,000" description="Suzuki Every Wagon 2011" totalRunning="137,000 km" model="2011" location="Cantt, Karachi" adPosted="5 days ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/486403677-800x600.webp" price="2,100,000" description="Toyota Vitz 2013" totalRunning="170,000" model="2013" location="Shah Rukn-e-Alam Colony, Multan" adPosted="2 days ago"/>
+    <CARDBODY imgsrc="https://images.olx.com.pk/thumbnails/491109304-800x600.webp" price="820,000" description="Suzuki Cultus VXL 2005" totalRunning="80,000" model="2005" location="Gulistan-e-Jauhar Block 12, Karachi" adPosted="1 day ago"/>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,17 @@ import Card from 'react-bootstrap/Card';
 function CARDBODY ({price , description , totalRunning , model , location , adPosted , imgsrc}) {
   return (
     <Card className='Card px-0' style={{ width: '300px' }}>
-      <Card.Img className='w-100 border border-0' style={{height: '150px' , borderRadius: '0px'}} src="https://via.placeholder.com/500" />
+      <Card.Img className='w-100 border border-0' style={{height: '150px' , borderRadius: '0px' , objectFit: 'cover'}} src={imgsrc} />
       <Card.Body className='px-3 d-flex flex-column'>
         <div className='d-flex justify-content-between'>
-        <h6 className='fw-bold'>Rs. 980,000</h6>
+        <h6 className='fw-bold'>Rs. {price}</h6>
         <h5><i class="fa-regular fa-heart"></i></h5>
         </div>
-        <p>Suzuki Mehran VXR Model 2013 better than baleno liana coure</p>
+        <p>{description}</p>
         <div>
-        <p className='fw-bold d-flex justify-content-start align-items-center gap-1' style={{fontSize: '0.75rem'}}><i class="fa-solid fa-gauge-high"></i>200,000 km <i class="fas fa-dot-circle" style={{fontSize: '2.5px'}}></i> 2013</p>
-        <p style={{fontSize: '0.90rem', marginTop:'-15px'}}>Oasis Orchard, Faisalabad</p>
-        <p style={{fontSize: '0.75rem' , marginTop: '-15px' , marginBottom: '0px'}}>1 week ago</p>
+        <p className='fw-bold d-flex justify-content-start align-items-center gap-1' style={{fontSize: '0.75rem'}}><i class="fa-solid fa-gauge-high"></i>{totalRunning} km <i class="fas fa-dot-circle" style={{fontSize: '2.5px'}}></i> {model}</p>
+        <p style={{fontSize: '0.90rem', marginTop:'-15px'}}>{location}</p>
+        <p style={{fontSize: '0.75rem' , marginTop: '-15px' , marginBottom: '0px'}}>{adPosted}</p>
         </div>
       </Card.Body>
     </Card>
